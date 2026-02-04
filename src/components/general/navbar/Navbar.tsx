3d13172bf7da1751234f517e66633e8ff3436de0
Lethabo <lethabo20t.phofa02@gmail.com>
2026-02-04 13:36:27 +0200

    `Navbar completion + mobile version`

--- src/components/general/navbar/Navbar.tsx
+++ src/components/general/navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+"use client"
 import Link from "next/link";
 import Logo from "./Logo";
+import { LuMenu, LuNotebookPen, LuSearch, LuX } from "react-icons/lu";
+import MobileNavbar from "./MobileNavbar";
+import { useState } from "react";
 
-const navLinks = [
+export const navLinks = [
   {url:"/", label:"Home"},
   {url:"/articles", label:"Articles"},
   {url:"/about", label:"About"}
@@ -9,6 +13,7 @@ const navLinks = [
 ]
 
 export default function Navbar() {
+const [menuOpen, setMenuOpen] = useState(false); {/*this will keep the hamburger menu closed by default*/}
   return (
     <nav className="h-18 fixed top-0 left-0 z-50 backdrop-blur-md backdrop-saturate-50 w-full">
       <div className="flex items-center justify-between h-full w-[90%] mx-auto">
@@ -16,15 +21,28 @@ export default function Navbar() {
         <Logo/>
         {/* navlinks */}
         <ul className="flex items-center gap-4 md:gap-8 text-gray-400 font-semibold">
+          <li className="cursor-pointer flex items-center gap-1">
+            <LuSearch size={25}/>
+            <span className="hidden md:block ">Search</span>
+          </li>
+          <li className="cursor-pointer flex items-center gap-1">
+            <LuNotebookPen size={20}/>
+            <span className="hidden md:block">Write</span>
+          </li>
           {navLinks.map((link) => {
             return (
-              <li key={link.url}>
+              <li key={link.url} className="hidden md:block hover:text-gray-200">
                 <Link href={link.url}>{link.label}</Link>
               </li>
             )
           })}
+          <li className="bg-primary text-gray-200 px-3 lg:px-5 py-2 rounded-full cursor-pointer">Login</li>
+          <li className="cursor-pointer md:hidden z-80" onClick={()=> setMenuOpen(!menuOpen)}>
+            {menuOpen ? <LuX size={25}/>: <LuMenu size={25}/>}
+            </li>
         </ul>
       </div>
+      <MobileNavbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
     </nav>
   )
 }
